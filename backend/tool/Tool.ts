a5f8d3e27491d4b6dd7e30874246cc2ff27443c3
antoniogamizbadger <antonio.gamiz@badgermapping.com>
2025-03-10 21:59:25 -0600

    :hammer: uses v7 instead

--- backend/tool/Tool.ts
+++ backend/tool/Tool.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod';
+import { v7 as uuidv7 } from 'uuid';
 
 const ToolSchema = z.object({
-  id: z.string(),
+  id: z
+    .string()
+    .uuid()
+    .default(() => uuidv7()),
   name: z.string(),
 });
 
--- backend/tool/ToolRepository.test.ts
+++ backend/tool/ToolRepository.test.ts
@@ -1,19 +1,23 @@
 import { ToolRepository } from './ToolRepository';
+import { v7 as uuidv7 } from 'uuid';
+import type { Tool } from './Tool';
 
 describe('ToolRepository', () => {
   it('should add a tool', () => {
     const toolRepository = new ToolRepository();
+    const tool: Tool = { id: uuidv7(), name: 'Tool 1' };
 
-    toolRepository.addTool({ id: '1', name: 'Tool 1' });
+    toolRepository.addTool(tool);
 
-    expect(toolRepository.getTools()).toEqual([{ id: '1', name: 'Tool 1' }]);
+    expect(toolRepository.getTools()).toEqual([tool]);
   });
 
   it('should get all tools', () => {
     const toolRepository = new ToolRepository();
+    const tool: Tool = { id: uuidv7(), name: 'Tool 1' };
 
-    toolRepository.addTool({ id: '1', name: 'Tool 1' });
+    toolRepository.addTool(tool);
 
-    expect(toolRepository.getTools()).toEqual([{ id: '1', name: 'Tool 1' }]);
+    expect(toolRepository.getTools()).toEqual([tool]);
   });
 });
